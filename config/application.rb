c5c566e60a19cc69c00fa71c4f67d76077159567
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-04-24 17:06:01 -0400

    fixed it. now excuse me while I go punch a baby panda

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module Railsgoat
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # add app/assets/fonts to the asset path
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
