db19e5d990c6d402a8baa8eb44770ce53da39545
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-04-25 13:40:50 -0400

    went ahead and filled in XSS

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -10,9 +10,19 @@ class TutorialsController < ApplicationController
   end
   
   def injection
+  
   end
   
   def xss
+      	@code = %{
+  					<li style="color: #FFFFFF">
+  						<!-- 
+  						I'm going to use HTML safe because we had some weird stuff
+  						going on with funny chars and jquery, plus it says safe so I'm guessing
+  						nothing bad will happen
+  						-->
+  						Welcome, <%= current_user.first_name.html_safe %>
+  			   		</li>}
   end
   
   def broken_auth
