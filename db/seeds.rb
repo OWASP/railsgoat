b1b378f451477cf313ac17a5b626a952330e6236
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-05-24 19:34:58 -0400

    added seed data, now time to work on the controllers and the view

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,14 +4,80 @@
 
 
 users = [
-     {:email => "admin@metacorp.com", :admin => true, :password => "admin1234", :first_name => "Admin", :last_name => "", :user_id =>1 },
-     {:email => "jack@metacorp.com", :admin => false, :password => "yankeessuck", :first_name => "Jack", :last_name => "Mannino", :user_id => 2},
-     {:email => "jim@metacorp.com", :admin => false, :password => "alohaowasp", :first_name => "Jim", :last_name => "Manico", :user_id =>3 },
-     {:email => "mike@metacorp.com", :admin => false, :password => "motorcross1445", :first_name => "Mike", :last_name => "McCabe", :user_id =>4 },
-     {:email => "ken@metacorp.com", :admin => false, :password => "citrusblend", :first_name => "Ken", :last_name => "Johnson", :user_id =>5 }
-  
+     {
+       :email => "admin@metacorp.com", 
+       :admin => true, :password => "admin1234", 
+       :first_name => "Admin", 
+       :last_name => "", 
+       :user_id =>1 
+      },
+     {
+       :email => "jack@metacorp.com", 
+       :admin => false, 
+       :password => "yankeessuck", 
+       :first_name => "Jack", 
+       :last_name => "Mannino", 
+       :user_id => 2
+     },
+     {
+       :email => "jim@metacorp.com", 
+       :admin => false, 
+       :password => "alohaowasp", 
+       :first_name => "Jim", 
+       :last_name => "Manico", 
+       :user_id =>3 
+     },
+     {
+       :email => "mike@metacorp.com", 
+       :admin => false, 
+       :password => "motorcross1445", 
+       :first_name => "Mike", 
+       :last_name => "McCabe", 
+       :user_id =>4 
+      },
+     {
+       :email => "ken@metacorp.com", 
+       :admin => false, 
+       :password => "citrusblend", 
+       :first_name => "Ken", 
+       :last_name => "Johnson", 
+       :user_id =>5 
+      }
 ]
+
+retirements = [
+      {
+        :user_id => "2",
+        :employee_contrib => "1000",
+        :employer_contrib => "2000",
+        :total => "4500"
+      },
+      {
+        :user_id => "3",
+        :employee_contrib => "8000",
+        :employer_contrib => "16000",
+        :total => "30000"
+      },
+      {
+        :user_id => "4",
+        :employee_contrib => "10000",
+        :employer_contrib => "20000",
+        :total => "40000"
+      },
+      {
+        :user_id => "5",
+        :employee_contrib => "3000",
+        :employer_contrib => "6000",
+        :total => "12500"
+      },
+      
+]
+
 
 users.each do |user_info|
   User.create!(user_info)
 end
+
+retirements.each do |r|
+ Retirement.create!(r)
+end  
