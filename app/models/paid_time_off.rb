9feae35f5f99ed2e7cab902105d38794d613c62c
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-05-28 09:44:17 -0400

    switching to a different graph

--- app/models/paid_time_off.rb
+++ app/models/paid_time_off.rb
@@ -3,18 +3,4 @@ class PaidTimeOff < ActiveRecord::Base
   belongs_to :user
   has_one :schedule, :foreign_key => :user_id, :primary_key => :user_id
 
-  # Refactor this duplication when it's not 3am
-  def sick_days_remaining(val="")
-      self.sick_days_earned - self.sick_days_taken
-  end
-  
- 
-  def pto_days_remaining(val="")
-      self.pto_earned - self.pto_taken
-  end
-  
-  def pto_percentages
-  end
-
-  
 end
