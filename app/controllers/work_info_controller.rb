14251e6f397b1d03ae176092fcedaaa321fca354
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-06-03 01:29:16 -0400

    added Insecure dor vuln

--- app/controllers/work_info_controller.rb
+++ app/controllers/work_info_controller.rb
@@ -1,6 +1,11 @@
 class WorkInfoController < ApplicationController
   
   def index
-    @user = current_user
+    @user = User.find_by_user_id(params[:user_id])
+    if !(@user)
+      flash[:error] = "Sorry, no user with that user id exists"
+      redirect_to home_dashboard_index_path
+    end
   end
+  
 end
