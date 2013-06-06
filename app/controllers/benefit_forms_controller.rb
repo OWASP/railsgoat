d445e59a98aa3dc46954f46ea9bb26785b68a2bb
Ken Johnson <cktricky@Kens-MacBook-Pro.local>
2013-06-06 16:43:58 -0400

    this fixes issue #20, seriously, no clue how I missed the missing constantize code

--- app/controllers/benefit_forms_controller.rb
+++ app/controllers/benefit_forms_controller.rb
@@ -2,31 +2,39 @@ class BenefitFormsController < ApplicationController
   
   def index
   end
-  
+
+
   def download
    begin  
-     file = Rails.root.join('public', 'docs', params[:name])
+     path = Rails.root.join('public', 'docs', params[:name])
+     file = params[:type].constantize.new(path)
      send_file file, :disposition => 'attachment'
    rescue
      redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
    end
   end
+
   
-=begin
+=begin  
     # More secure version
     def download
      file_assoc = {"1" => "Health_n_Stuff.pdf", "2" => "Dental_n_Stuff.pdf"}
      begin  
        if file_assoc.has_key?(params[:name].to_s)
-          file = Rails.root.join('public', 'docs', file_assoc[params[:name].to_s])
-          send_file file, :disposition => 'attachment'
+          path = Rails.root.join('public', 'docs', file_assoc[params[:name].to_s])
+          if params[:type] == "File"
+            file = params[:type].constantize.new(path)  
+            send_file file, :disposition => 'attachment'
+          end 
        else 
          file =  Rails.root.join('public', 'docs', "Dental_n_Stuff.pdf")
+         send_file file, :disposition => 'attachment'
        end
      rescue
        redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
      end
     end
-=end  
+=end      
+
   
 end
