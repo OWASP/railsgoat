c3562592c6a3522f26d8cb5dd5520667bf0f81cd
cktricky <ken.johnson@nvisiumsecurity.com>
2013-09-27 11:17:16 -0400

    deleted some files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user, :is_admin?
   
   # Our security guy keep talking about sea-surfing, cool story bro.
-  # protect_from_forgery
+   protect_from_forgery
   
   private
 
