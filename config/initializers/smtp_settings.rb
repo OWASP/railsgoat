93d7c2bd4407f1e3600253b848c20bf393ab1a7f
James Espinosa <jamesejr@gmail.com>
2013-11-24 23:57:52 -0600

    Add mailtrap.io SMTP settings

---
+++ config/initializers/smtp_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+  :address           => 'mailtrap.io',
+  :port              => 2525,
+  :authentication    => :plain,
+  :user_name         => ENV['MAILTRAP_USERNAME'],
+  :password          => ENV['MAILTRAP_PASSWORD'],
+}
+
+ActionMailer::Base.delivery_method = :smtp
