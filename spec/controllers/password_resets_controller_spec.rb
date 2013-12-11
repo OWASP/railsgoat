bfa3467107c1a334a495a10a97d3f4c7e989664f
James Espinosa <jamesejr@gmail.com>
2013-12-10 23:08:46 -0600

    Remove default RSpec tests to fix build

--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -1,12 +1,1 @@
 require 'spec_helper'
-
-describe PasswordResetsController do
-
-  describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
-  end
-
-end
