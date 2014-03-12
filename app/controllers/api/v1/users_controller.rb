f4f5d5744cc064a3612d87bb885061d5a09d96f3
cktricky <ken.johnson@nvisiumsecurity.com>
2014-03-12 13:24:37 -0400

    working on the auth structure for the API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,11 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   
    skip_before_filter :authenticated
-     
+   before_filter :valid_api_token
+  
      respond_to :json
      
-     def index
-       respond_with ({:hi => :world})
+     def valid_api_token
+       authenticate_or_request_with_http_token do |token, options|
+        # TODO :add some functionality to check if the HTTP Header is valid
+        return true
+       end
      end
-  
+     
+     def index
+      respond_with User.all
+     end
+     
+     
 end
