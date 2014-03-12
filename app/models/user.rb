4e6006dcc88049a3fbcb45bc6ed96d74371b3f41
relotnek <kentoler@Kenneths-MacBook-Pro.local>
2014-03-11 20:29:43 -0400

    added before_create generate token to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   has_one :work_info, :foreign_key => :user_id, :primary_key => :user_id, :dependent => :destroy
   has_many :performance, :foreign_key => :user_id, :primary_key => :user_id, :dependent => :destroy
   has_many :messages, :foreign_key => :receiver_id, :primary_key => :user_id, :dependent => :destroy
-
+  before_create { generate_token(:auth_token) }
 
   def build_benefits_data
     build_retirement(POPULATE_RETIREMENTS.shuffle.first)
