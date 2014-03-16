1f922916d27833de2a5ffd37d05a25072bf9b767
cktricky <ken.johnson@nvisiumsecurity.com>
2014-03-15 21:58:42 -0400

    have the ability now to update a row of direct deposit information as well as leverage the encryption routine to introduce a serious flaw

--- app/controllers/pay_controller.rb
+++ app/controllers/pay_controller.rb
@@ -4,9 +4,23 @@ class PayController < ApplicationController
   end
   
   def update_dd_info
+    msg = false
+    pay = Pay.new(
+    :bank_account_num => params[:bank_account_num], 
+    :bank_routing_num => params[:bank_routing_num], 
+    :percent_of_deposit => params[:dd_percent]
+    )
+    pay.user_id = current_user.user_id
+    msg = true if pay.save! 
     respond_to do |format|
-      format.json {render :json => {:hello => :world} }
+      format.json {render :json => {:msg => :world} }
     end
+  end
+  
+  def show
+   respond_to do |format|
+     format.json { render :json => {:user => current_user.pay.as_json} }
+   end
   end
   
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,9 @@ Railsgoat::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Tired of caching causing issues
+  config.middleware.delete Rack::ETag
 
   # Do not compress assets
   config.assets.compress = false
