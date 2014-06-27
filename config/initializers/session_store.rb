2a12765933b4784741daea4e4a04f4f95915478d
cktricky <ken.johnson@nvisiumsecurity.com>
2014-06-27 12:05:50 -0400

    slight change to make our cookie even more insecure

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Railsgoat::Application.config.session_store :cookie_store, key: '_railsgoat_session'
+Railsgoat::Application.config.session_store :cookie_store, key: '_railsgoat_session', httponly: false
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
