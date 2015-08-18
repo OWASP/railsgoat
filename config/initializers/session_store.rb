5c62c1b0216c9118964d9fb496d7459d3858c51f
cktricky <cktricky@gmail.com>
2015-08-18 12:27:20 -0400

    the setting was incorrect and did not match what we show in the tutorial

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Railsgoat::Application.config.session_store :cookie_store, key: '_railsgoat_session'
+Railsgoat::Application.config.session_store :cookie_store, key: '_railsgoat_session', httponly: false
