67403fcdcd15b8c681de634cff54b73156dd765e
cktricky <cktricky@gmail.com>
2015-12-25 19:53:55 -0500

    Due to a previous migration file which already implemented an email column within the user table, I did a hacky workaround by commenting out the conflicting column entry within the newly generated devise migration file.

--- db/migrate/20151224160152_add_devise_to_users.rb
+++ db/migrate/20151224160152_add_devise_to_users.rb
@@ -2,7 +2,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
     change_table(:users) do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      #t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -35,7 +35,7 @@ class AddDeviseToUsers < ActiveRecord::Migration
       # t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    #add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
