46769d31fe5f4abb7a780201f5ca6e64659f5563
cktricky <cktricky@gmail.com>
2015-12-29 17:45:36 -0500

    fullcalendar.js was required in the assets compilation when viewing any section that had a calendar

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( validation.js jquery.dataTables.js )
+Rails.application.config.assets.precompile += %w(  fullcalendar.js validation.js jquery.dataTables.js )
