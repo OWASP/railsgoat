aaac827a02fa737414935e1dfab1b8239ad7ce15
cktricky <cktricky@gmail.com>
2016-01-01 19:22:26 -0500

    doh, there was  reason this was still included. It is purely to show credentials

---
+++ app/controllers/tutorials_controller.rb
@@ -0,0 +1,9 @@
+class TutorialsController < ApplicationController
+  skip_before_filter :has_info
+  skip_before_filter :authenticate_user!
+
+  def credentials
+    render :partial => "layouts/tutorial/credentials/creds"
+  end
+
+end
