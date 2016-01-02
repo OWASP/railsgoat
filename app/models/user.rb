d117fca8df944767b659d5b2c88998aa1d45f42d
cktricky <cktricky@gmail.com>
2016-01-01 19:23:57 -0500

    removed some unnecessary stuff and added validations for first name and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :password, :presence => true,
-                       :confirmation => true,
-                       :length => {:within => 6..40},
-                       :on => :create,
-                       :if => :password
 
-  validates_presence_of :email
+  validates_presence_of :email, :first_name, :last_name
   validates_uniqueness_of :email
   validates_format_of :email, :with => /.+@.+\..+/i
   attr_accessor :skip_user_id_assign
