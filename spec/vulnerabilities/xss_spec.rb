b215f49414052fe77039f843c61606f5ebac3360
cktricky <cktricky@gmail.com>
2016-01-05 11:01:21 -0500

    An account update will require you to sign in now if you update the password (devise). Previously, we had an issue where we had to submit the password field or risk the test failing (poor update code to blame). So because we entered a password, we now needed to sign in again, which broke the test. Luckily, we do not need to enter a password anymore so I removed the password field entries

--- spec/vulnerabilities/xss_spec.rb
+++ spec/vulnerabilities/xss_spec.rb
@@ -12,17 +12,12 @@ feature 'xss' do
     visit "/users/#{@normal_user.user_id}/account_settings"
     within('#account_edit') do
       fill_in 'First name', :with => "<script>$(function() { $('div input.btn').val('RailsGoat h4x0r3d') } )</script>"
-
-      # password gets screwed up if you don't re-submit - need to fix
-      fill_in 'user_password', :with => @normal_user.clear_password
-      fill_in 'user_password_confirmation', :with => @normal_user.clear_password
     end
     click_on 'Submit'
 
     sleep(1)
-
+    
     visit "/users/#{@normal_user.user_id}/account_settings"
-
     pending(:if => verifying_fixed?) { find('#submit_button').value.should == 'RailsGoat h4x0r3d' }
 
     # might be nice to demonstrate posting cookie contents or somesuch, but
