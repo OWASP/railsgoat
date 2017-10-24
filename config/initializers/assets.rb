6258fc2b21d51e49721b09ed2707f8d9a8283747
Joseph Mastey <jmmastey@gmail.com>
2017-10-23 21:10:22 -0500

    update fullcalendar js library, plus styles etc

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( validation.js jquery.dataTables.min.js fullcalendar.js )
+Rails.application.config.assets.precompile += %w( validation.js jquery.dataTables.min.js fullcalendar.min.js moment.min.js )
