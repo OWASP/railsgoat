7d527170bda52e285adc97a1924ca88c0b5b9eec
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2019-11-20 09:24:24 -0500

    clean up insecure_dor_spec

--- spec/vulnerabilities/insecure_dor_spec.rb
+++ spec/vulnerabilities/insecure_dor_spec.rb
@@ -18,7 +18,7 @@ feature "insecure direct object reference" do
     visit download_url.sub(/name=(.*?)&/, "name=config/database.yml&")
 
     expect(page.status_code).not_to eq(200)
-    expect(page.response_headers["Content-Disposition"]).not_to include("database.yml")
+    expect(page.response_headers["Content-Disposition"].to_a).not_to include("database.yml")
   end
 
   scenario "attack two\nTutorial: https://github.com/OWASP/railsgoat/wiki/A4-Insecure-Direct-Object-Reference" do
