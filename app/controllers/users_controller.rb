4ce6b080352fafbb7b5c40db76e11d6c83030919
François Proulx <francois@boostsecurity.io>
2021-09-21 16:53:21 -0400

    Drop SQLis

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,9 @@ class UsersController < ApplicationController
 
     user = User.where("id = '#{params[:user][:id]}'")[0]
 
+    # Another SQLi!!!!
+    user = User.where("id = '#{params[:user][:id]}'")[0]
+
     if user
       user.update(user_params_without_password)
       if params[:user][:password].present? && (params[:user][:password] == params[:user][:password_confirmation])
